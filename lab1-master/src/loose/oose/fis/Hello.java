83b3eaf58dd1a386b3f09ae8e63d95266263f8b1
Alexandra Hertanu <alexandra.hertanu@yahoo.com>
2020-03-19 16:51:11 +0200

    "Added one more file and modified file"

---
+++ lab1-master/src/loose/oose/fis/Hello.java
@@ -0,0 +1,5 @@
+class Hello{
+    public void afis(){
+        System.out.println("Hi!");
+    }
+}
--- lab1-master/src/loose/oose/fis/documents/JSON.java
+++ lab1-master/src/loose/oose/fis/documents/JSON.java
@@ -10,16 +10,16 @@ public class JSON extends Document {
     @Override
     public String[] analizeaza() {
         String[] res = new String[continut.length];
-        int      pos = 0;
+        int      p = 0;
 
         for (String cuvant : continut) {
             if (!cuvant.contains(":")) {
-                res[pos] = cuvant;
-                pos++;
+                res[p] = cuvant;
+                p++;
             }
         }
 
-        return Arrays.copyOf(res, pos);
+        return Arrays.copyOf(res, p);
     }
 
     @Override
